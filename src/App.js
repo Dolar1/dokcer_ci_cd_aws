6123182648e3083f27fbe0085e2683f54eb71963
Dolar1 <dolarsingh111@gmail.com>
2020-06-24 18:39:08 +0630

    change in feature...

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>FUCKING CRAZY I HAVE DONE IT ALL ONLINE IN ONE SHOT...</h1>
         <p>DOLAR'S DOCKER FILE. SERIOUSLY</p>
       </header>
     </div>
